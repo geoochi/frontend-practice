ab5a84d32a1d6da01a5504d0b6bfc73de4e66a4b
Geoo Chi <geoooochi@gmail.com>
2024-11-29 13:02:23 +0800

    update

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <p>
+        inspire by <a href='https://jenniferdewalt.com/'>180 websites</a> and{' '}
+        <a href='https://levels.io/12-startups-12-months/'>12 startups</a>
+      </p>
+    </>
+  )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,84 +1,26 @@
 import { StrictMode, Suspense, lazy, Component } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router'
+import { HashRouter, Routes, Route, Link } from 'react-router'
+import Home from './components/Home.jsx'
+import HelloWorld from './components/HelloWorld.jsx'
+import ModuleCSS from './components/ModuleCSS.jsx'
 import './assets/index.css'
-import pages from './assets/pages.json'
-
-class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { hasError: false }
-  }
-
-  static getDerivedStateFromError(error) {
-    return { hasError: true }
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <NotFound name={this.props.name} />
-    }
-    return this.props.children
-  }
-}
-
-const NotFound = ({ name }) => (
-  <div
-    style={{
-      textAlign: 'center',
-      padding: '40px',
-      color: '#666',
-    }}
-  >
-    {name} Not Found
-  </div>
-)
-
-const DynamicComponent = ({ name }) => {
-  const Component = lazy(() => import(`./components/${name}.jsx`))
-  return (
-    <ErrorBoundary name={name}>
-      <Suspense>
-        <Component />
-      </Suspense>
-    </ErrorBoundary>
-  )
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div id='sidebar'>
-      {pages.map((item) => (
-        <div key={item}>{item}</div>
-      ))}
-    </div>
-    <div id='container'>
-      <HashRouter>
+    <HashRouter>
+      <div id='sidebar'>
+        <Link to='/'>Home</Link>
+        <Link to='/HelloWorld'>Hello World</Link>
+        <Link to='/ModuleCSS'>ModuleCSS</Link>
+      </div>
+      <div id='container'>
         <Routes>
-          <Route
-            path='/'
-            element={
-              <p>
-                inspire by{' '}
-                <a href='https://jenniferdewalt.com/'>180 websites</a> and{' '}
-                <a href='https://levels.io/12-startups-12-months/'>
-                  12 startups
-                </a>
-              </p>
-            }
-          />
-
-          {pages.map((item) => (
-            <Route
-              key={item}
-              path={`/${item.toLowerCase()}`}
-              element={<DynamicComponent name={item} />}
-            />
-          ))}
-
-          <Route path='*' element={<NotFound name='Page' />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/ModuleCSS' element={<ModuleCSS />} />
+          <Route path='/HelloWorld' element={<HelloWorld />} />
         </Routes>
-      </HashRouter>
-    </div>
+      </div>
+    </HashRouter>
   </StrictMode>
 )
