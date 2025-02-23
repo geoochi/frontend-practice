65f124d80f6f64f297b684f64d2cdb6f5c076d5f
Geoo Chi <geoochi@geoochi.com>
2025-02-23 21:41:51 +0800

    update sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,12 +7,12 @@ const Sidebar: React.FC = () => {
   return (
     <div
       className={cn(
-        'flex flex-col w-[200px] h-full',
+        'flex flex-col justify-center w-[200px] h-full',
         'bg-[#f0f0f0] dark:bg-[#111] overflow-y-auto',
         'transition-colors duration-300'
       )}
     >
-      <div className='p-[10px] my-[10px] flex justify-center'>
+      <div className='p-[20px] my-[10px] flex justify-center'>
         <LightDark />
       </div>
       {routes.map(
@@ -22,7 +22,7 @@ const Sidebar: React.FC = () => {
               key={route.path}
               to={route.path}
               className={cn(
-                'p-[10px] my-[10px] mx-[20px] text-center rounded-[5px]',
+                'p-[20px] my-[10px] mx-[20px] text-center rounded-[5px]',
                 'decoration-[none] hover:bg-[#e0e0e0] dark:hover:bg-[#222]'
               )}
             >
