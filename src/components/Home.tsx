37dcf9f8e986c0eac53a99f8d8d71ef2d59976f3
Geoo Chi <geoochi@geoochi.com>
2025-02-23 22:23:01 +0800

    update Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,8 +2,21 @@ const Home: React.FC = () => {
   return (
     <p>
       This is a practice project inspired by{' '}
-      <a href='https://jenniferdewalt.com/'>180 websites</a> and{' '}
-      <a href='https://levels.io/12-startups-12-months/'>12 startups</a>
+      <a
+        href='https://jenniferdewalt.com/'
+        target='_blank'
+        className='underline'
+      >
+        180 websites ↗
+      </a>{' '}
+      and{' '}
+      <a
+        href='https://levels.io/12-startups-12-months/'
+        target='_blank'
+        className='underline'
+      >
+        12 startups ↗
+      </a>
     </p>
   )
 }
